5ce968fc1083e691a658895c750374ab3c33c365
rgurgul <gurgul.robert@gmail.com>
2024-11-19 11:07:43 +0100

    x

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./projects/apka/src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
