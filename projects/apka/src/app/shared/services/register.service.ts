7321174f01a77073a9862ff02c2909cf7631ab4f
rgurgul <gurgul.robert@gmail.com>
2024-11-19 08:48:59 +0100

    x

--- projects/apka/src/app/shared/services/register.service.ts
+++ projects/apka/src/app/shared/services/register.service.ts
@@ -2,13 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { FieldConfig } from '../types/form.types';
+
 import { Api } from '../utils/api';
 
 @Injectable({ providedIn: 'root' })
 export class RegisterService {
   http = inject(HttpClient);
   formConfig$: Observable<any> = this.http
-    .get<{ data: FieldConfig[] }>(Api.DATA_FORM_CONFIG)
-    .pipe(map((resp: { data: FieldConfig[] }) => resp.data));
+    .get<{ data: any[] }>(Api.DATA_FORM_CONFIG)
+    .pipe(map((resp: { data: any[] }) => resp.data));
 }
