d510570f3be6e8afc69cbc32cfe1198964c44645
rgurgul <gurgul.robert@gmail.com>
2024-11-20 14:00:36 +0100

    x

--- projects/auth/src/app/app.component.ts
+++ projects/auth/src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
+  template: `
+    <form [formGroup]="form" (submit)="onSubmit($event)">
+      <input type="text" formControlName="username" />
+      <br>
+      <input type="text" formControlName="password" />
+      <br>
+      <button>send</button>
+    </form>
+    {{ form.value | json }}
+  `,
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  onSubmit($event: SubmitEvent) {
+    console.log(this.form.valid);
+  }
   title = 'auth';
+  fb = inject(FormBuilder);
+  form: FormGroup = this.fb.group({});
+
+  constructor() {
+    this.form.addControl('username', this.fb.control('bob'));
+    this.form.addControl('password', this.fb.control(''));
+  }
 }
